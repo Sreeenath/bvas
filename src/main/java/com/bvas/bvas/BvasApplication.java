6642ca5f115403c67e75d884d71626b061569071
Sreenath M <sreenathmalamel@gmail.com>
2026-01-19 11:05:49 +0530

    First Commit

---
+++ src/main/java/com/bvas/bvas/BvasApplication.java
@@ -0,0 +1,13 @@
+package com.bvas.bvas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BvasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BvasApplication.class, args);
+	}
+
+}
